b958fa9c5f3f143e2141a49e592628dd8e951948
HugoIP <bairon.myh@gmail.com>
2021-07-12 20:25:43 -0500

    Varsion uploaded to server DB

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=tickets;charset=utf8', 'root', '');
+        $pdo = new PDO('mysql:host=localhost;dbname=tickets;charset=utf8', 'hugoip', 'MONICA');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
